4a1c24d29720c159251d76a06de59af4b63c4c9c
weixy2000 <wxy008@163.com>
2014-10-31 10:38:40 +0800

    wxy

--- JavaStudy/src/com/wxy/java/beans/IntrospectorTest.java
+++ JavaStudy/src/com/wxy/java/beans/IntrospectorTest.java
@@ -12,7 +12,8 @@ public class IntrospectorTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("test123");
+		System.out.println("test1");
+		System.out.println("test2");
 	}
 
 }
