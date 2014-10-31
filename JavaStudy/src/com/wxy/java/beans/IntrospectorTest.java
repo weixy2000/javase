dffb5ab2172d463526b1ba614d0b79aab5b845ba
weixy2000 <wxy008@163.com>
2014-10-31 09:46:21 +0800

    wxy

--- JavaStudy/src/com/wxy/java/beans/IntrospectorTest.java
+++ JavaStudy/src/com/wxy/java/beans/IntrospectorTest.java
@@ -1,10 +1,18 @@
 package com.wxy.java.beans;
 
 /**
- * Java 内省机制 
+ * Java 内省机制
  * @author xiangyang.wei
  * @since 2014年10月31日上午9:38:05
  */
 public class IntrospectorTest {
+
+	/**
+	 * 主程序入口
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
 
 }
